cd32987ed537322c544a13025428675d403d6d9d
SKYWARN <simplepttusa@gmail.com>
2025-05-30 14:50:41 -0500

    Add all files

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# nexrad-viewer
+# NEXRAD Radar Viewer
+
+## How to use
+
+1. Clone or download this repo.
+2. Run `npm install`
+3. Run `npm start`
+4. Open [http://localhost:3000](http://localhost:3000)
+5. To embed in an iframe on your website, use:
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/radar/:site/:product/:frame', async (req, res) => {
+  const { site, product, frame } = req.params;
+  const url = `https://radar.weather.gov/ridge/RadarImg/${product}/${site}_${product}_${frame}.png`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) return res.status(404).send('Not found');
+    res.set('Content-Type', 'image/png');
+    response.body.pipe(res);
+  } catch (err) {
+    res.status(500).send('Proxy fetch error');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
